63e61e32669a610bfe28f944fd8ef76c1b4ace7a
MOHAMMAD EHTESHAM <infoehtesham7886@gmail.com>
2026-07-16 23:59:23 +0530

    Time: 7 ms (69.4%), Space: 44 MB (61.8%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] s1Freq = new int[26];
+        int[] windowFreq = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            s1Freq[s1.charAt(i) - 'a']++;
+            windowFreq[s2.charAt(i) - 'a']++;
+        }
+
+        if (matches(s1Freq, windowFreq)) {
+            return true;
+        }
+
+        for (int right = s1.length(); right < s2.length(); right++) {
+
+            windowFreq[s2.charAt(right) - 'a']++;
+
+            windowFreq[s2.charAt(right - s1.length()) - 'a']--;
+
+            if (matches(s1Freq, windowFreq)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean matches(int[] a, int[] b) {
+
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

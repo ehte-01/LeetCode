779c9481fefdde9318062f6f98937d13a9ae4602
MOHAMMAD EHTESHAM <infoehtesham7886@gmail.com>
2026-07-18 00:03:12 +0530

    Time: 8 ms (52.72%), Space: 44.1 MB (48.6%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,45 +1,42 @@
 class Solution {
-
     public boolean checkInclusion(String s1, String s2) {
-
-        if (s1.length() > s2.length()) {
+        if(s1.length()>s2.length()){
             return false;
         }
+        int[] S1Window = new int[26];
+        int[] Window = new int[26];
 
-        int[] s1Freq = new int[26];
-        int[] windowFreq = new int[26];
-
-        for (int i = 0; i < s1.length(); i++) {
-            s1Freq[s1.charAt(i) - 'a']++;
-            windowFreq[s2.charAt(i) - 'a']++;
+        for(int i=0; i<s1.length();i++){
+            S1Window[s1.charAt(i) -'a']++;
         }
 
-        if (matches(s1Freq, windowFreq)) {
-            return true;
-        }
-
-        for (int right = s1.length(); right < s2.length(); right++) {
-
-            windowFreq[s2.charAt(right) - 'a']++;
-
-            windowFreq[s2.charAt(right - s1.length()) - 'a']--;
-
-            if (matches(s1Freq, windowFreq)) {
-                return true;
+        int matches=0;
+        for(int i=0;i<26;i++){
+            if(Window[i]==S1Window[i]){
+                matches++;
             }
         }
 
+        int left=0;
+        for(int right=0;right<s2.length();right++){
+            int c = s2.charAt(right) -'a';
+
+            if(Window[c]==S1Window[c])
+            matches--;
+            Window[c]++;
+            if(Window[c]==S1Window[c])
+            matches++;
+        
+
+        if(right-left+1>s1.length()){
+            int d=s2.charAt(left) - 'a';
+            if(Window[d]==S1Window[d]) matches--;
+            Window[d]--;
+            if(Window[d]==S1Window[d]) matches++;
+            left++;
+        }
+        if(matches==26) return true;
+        }
         return false;
-    }
-
-    private boolean matches(int[] a, int[] b) {
-
-        for (int i = 0; i < 26; i++) {
-            if (a[i] != b[i]) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
